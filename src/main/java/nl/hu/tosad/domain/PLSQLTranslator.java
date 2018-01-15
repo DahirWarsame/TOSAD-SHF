a8a6edf2d6804901a08f3be2abbba79260e8b395
Gundraub <r.e.v.t.noten@gmail.com>
2018-01-15 13:58:02 +0100

    Added PLSQLTranslator
    Consists (for now) of a sorry excuse for a Demo method, proving that the communication with the BR server functions and that its contents can be interpreted.

---
+++ src/main/java/nl/hu/tosad/domain/PLSQLTranslator.java
@@ -0,0 +1,41 @@
+package nl.hu.tosad.domain;
+
+public class PLSQLTranslator {
+
+    public PLSQLTranslator() {
+
+    }
+
+//    public String generateConstraint(Rule rule, String name) {
+//        String result = "CREATE OR REPLACE TRIGGER " + name;
+//        result += "\nAFTER " + rule.getTrigger();
+//        result += "\nON " + rule.getTable();
+//        result += "\nFOR EACH ROW";
+//
+//        String ruleCode = rule.getType().getCode() + rule.getFunction().getCode();
+//
+//        switch (ruleCode) {
+//            case "ARNG" :
+//                result += "\nDECLARE";
+//                result += "\nl_passed\tboolean := true;";
+//                result += "\nBEGIN";
+//                result += "\n";
+//                break;
+//        }
+//
+//        return result;
+//    }
+
+    public String generateDemo(Rule rule) {
+        String result = "";
+
+        result += "\nHERE BE CONSTRAINT OF TYPE " + rule.getType().getDesc() + "\n";
+        result += "\n\tAFTER " + rule.getTrigger();
+        result += "\n\tON " + rule.getTable();
+        result += "\n\tCONCERNING " + rule.getAttribute() + "\n";
+        result += "\nCORRECT AND COMPLETE PL SQL STATEMENT WILL FOLLOW SOON(TM)";
+
+        return result;
+    }
+
+}
--- src/main/java/nl/hu/tosad/webservices/Controller.java
+++ src/main/java/nl/hu/tosad/webservices/Controller.java
@@ -1,6 +1,7 @@
 package nl.hu.tosad.webservices;
 
 import nl.hu.tosad.domain.Other;
+import nl.hu.tosad.domain.PLSQLTranslator;
 import nl.hu.tosad.domain.RangeOperator;
 import nl.hu.tosad.domain.Rule;
 
@@ -23,31 +24,35 @@ public class Controller {
 
     private static void generatePLSQL(List<Rule> rules) {
 
+        PLSQLTranslator translator = new PLSQLTranslator();
+
         //TODO call SQL generator
 
         for (Rule rule : rules) {
-            String type = rule.getFunction().getCode();
-            if (type.equals("RNG")) {
-                RangeOperator ro = (RangeOperator) rule.getFunction();
-                //TODO send data to SQL generator
-                System.out.println(rule.getType().getDesc());
-                System.out.println(rule.getTable());
-                System.out.println(type);
-                System.out.println(ro.getMax());
-                System.out.println(ro.getMin());
-                System.out.println(ro.getType());
-                //TODO return generated sql
-            }
-            if (type.equals("OTH")) {
-                Other o = (Other) rule.getFunction();
-
-                //TODO send function SQL generator
-                System.out.println(rule.getType().getDesc());
-                System.out.println(rule.getTable());
-                System.out.println(type);
-                System.out.println(o.getBody());
-                //TODO return generated sql
-            }
+            System.out.println(translator.generateDemo(rule));
+//            String type = rule.getFunction().getCode();
+//            System.out.println(rule);
+//            if (type.equals("RNG")) {
+//                RangeOperator ro = (RangeOperator) rule.getFunction();
+//                //TODO send data to SQL generator
+////                System.out.println(rule.getType().getDesc());
+////                System.out.println(rule.getTable());
+////                System.out.println(type);
+////                System.out.println(ro.getMax());
+////                System.out.println(ro.getMin());
+////                System.out.println(ro.getType());
+//                //TODO return generated sql
+//            }
+//            if (type.equals("OTH")) {
+//                Other o = (Other) rule.getFunction();
+//
+//                //TODO send function SQL generator
+////                System.out.println(rule.getType().getDesc());
+////                System.out.println(rule.getTable());
+////                System.out.println(type);
+////                System.out.println(o.getBody());
+//                //TODO return generated sql
+//            }
         }
     }
 }
