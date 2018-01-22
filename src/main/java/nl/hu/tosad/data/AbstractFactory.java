720583ab40b5f1739838e784a96d7e0b24fd3f8e
graddus <luccagraddushuijgens@gmail.com>
2018-01-22 13:43:19 +0100

    Delete AbstractFactory.java

--- src/main/java/nl/hu/tosad/data/AbstractFactory.java
+++
@@ -1,6 +0,0 @@
-package nl.hu.tosad.data;
-
-public abstract class AbstractFactory {
-//	   RangeOperatorFactory getRangeOperator(String min, String max, String type);
-//	   CompareOperatorFactory getCompareOperator(String value, String type) ;
-}
