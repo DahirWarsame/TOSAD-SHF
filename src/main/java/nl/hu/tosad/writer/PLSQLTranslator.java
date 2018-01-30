494acf4d3442fad966e00d8a022ac8b9c08daf7b
Gundraub <r.e.v.t.noten@gmail.com>
2018-01-30 14:46:56 +0100

    Constraints added, and they might actually work!

--- src/main/java/nl/hu/tosad/writer/PLSQLTranslator.java
+++ src/main/java/nl/hu/tosad/writer/PLSQLTranslator.java
@@ -24,11 +24,70 @@ public class PLSQLTranslator implements Translator{
 	
 	
     private String generateConstraint(Rule rule, String name) {
-		StringBuilder result = new StringBuilder("ALTER TABLE " + rule.getTable());
+		StringBuilder result = new StringBuilder("ALTER TABLE");
+		result.append("\n\t" + rule.getTable());
+		result.append("\nADD CONSTRAINT");
+		result.append("\n\t" + name);
+        result.append("\n\tCHECK");
 
 		String ruleCode = rule.getType().getCode() + rule.getFunction().getCode();
 
 		switch (ruleCode) {
+            case "ARNG" :
+                RangeOperator attributeRangeFunction = ((RangeOperator) rule.getFunction());
+
+                result.append("\n\t(" + rule.getAttribute() + " " + translateOperator(attributeRangeFunction.getType()));
+                result.append(" " + attributeRangeFunction.getMin() + " AND " + attributeRangeFunction.getMax() + ")");
+                result.append("\n);");
+                break;
+
+            case "ACMP" :
+                CompareOperator attributeCompareFunction = ((CompareOperator) rule.getFunction());
+
+                result.append("\n\t(" + rule.getAttribute() + " ");
+                result.append(translateOperator(attributeCompareFunction.getType()) + " ");
+                if (isNumeric(attributeCompareFunction.getValue())) {
+                    result.append(attributeCompareFunction.getValue() + ")");
+                }
+                else {
+                    result.append("\'" + attributeCompareFunction.getValue() + "\')");
+                }
+                result.append("\n);");
+                break;
+
+            case "ALIS" :
+                ListOperator attributeListFunction = ((ListOperator) rule.getFunction());
+
+                result.append("\n\t(" + rule.getAttribute() + " " + translateOperator(attributeListFunction.getType()));
+                result.append("\n\t\t(");
+                int count = 0;
+                List<String> list = new ArrayList<String>(Arrays.asList(attributeListFunction.getList().split(",")));
+                for (String s : list) {
+                    count++;
+                    if (isNumeric(s)) {
+                        result.append("\n\t\t " + s);
+                    }
+                    else {
+                        result.append("\n\t\t \'" + s + "\'");
+                    }
+                    if (count < list.size()) {
+                        result.append(",");
+                    }
+                }
+                result.append("\n\t\t)");
+                result.append("\n\t)");
+                result.append("\n);");
+                break;
+
+            case "TCMP" :
+                CompareOperator tupleCompareFunction = ((CompareOperator) rule.getFunction());
+
+                result.append("\n\t(" + rule.getAttribute() + " ");
+                result.append(translateOperator(tupleCompareFunction.getType()) + " ");
+                result.append(rule.getType().getOtherattribute() + ")");
+                result.append("\n);");
+                break;
+
             default :
                 result = new StringBuilder("This constraint can currently not be translated to PL SQL");
                 break;
@@ -40,7 +99,7 @@ public class PLSQLTranslator implements Translator{
 	public String generateTrigger(Rule rule, String name) {
         StringBuilder result = new StringBuilder("CREATE OR REPLACE TRIGGER " + name);
         result.append("\nBEFORE UPDATE OR INSERT ");
-        result.append("\nON ").append(rule.getTable());
+        result.append("\nON " + rule.getTable());
         result.append("\nFOR EACH ROW");
 
         String ruleCode = rule.getType().getCode() + rule.getFunction().getCode();
@@ -50,30 +109,30 @@ public class PLSQLTranslator implements Translator{
                 RangeOperator attributeRangeFunction = ((RangeOperator) rule.getFunction());
 
                 result.append("\nBEGIN");
-                result.append("\n\tIF (NOT (:new."+rule.getAttribute());
-                result.append(" ").append(translateOperator(attributeRangeFunction.getType())).append(" ");
-                result.append(attributeRangeFunction.getMin()+" AND "+attributeRangeFunction.getMax() + "))");
+                result.append("\n\tIF (NOT (:new." + rule.getAttribute());
+                result.append(" " + translateOperator(attributeRangeFunction.getType()) + " ");
+                result.append(attributeRangeFunction.getMin() + " AND " + attributeRangeFunction.getMax() + "))");
                 result.append("\n\tTHEN RAISE_APPLICATION_ERROR(-20000, \'" + name + " was triggered\');");
                 result.append("\n\tEND IF;");
-                result.append("\nEND ").append(name).append(";");
+                result.append("\nEND " + name + ";");
                 break;
 
             case "ACMP" :
                 CompareOperator attributeCompareFunction = ((CompareOperator) rule.getFunction());
 
                 result.append("\nBEGIN");
-                result.append("\n\tIF (NOT (:new.").append(rule.getAttribute());
-                result.append(" ").append(translateOperator(attributeCompareFunction.getType())).append(" ");
-                if (isNumeric(attributeCompareFunction.getValue())){
+                result.append("\n\tIF (NOT (:new." + rule.getAttribute());
+                result.append(" " + translateOperator(attributeCompareFunction.getType()) + " ");
+                if (isNumeric(attributeCompareFunction.getValue())) {
                     result.append(attributeCompareFunction.getValue() + "))");
                 }
-                else{
-                    result.append("\'"+attributeCompareFunction.getValue() + "\'))");
+                else {
+                    result.append("\'" + attributeCompareFunction.getValue() + "\'))");
                 }
                 result.append("\n\tTHEN");
-                result.append("\n\tRAISE_APPLICATION_ERROR (-20001,\'").append(name).append(" was triggered\');");
+                result.append("\n\tRAISE_APPLICATION_ERROR (-20000,\'" + name + " was triggered\');");
                 result.append("\n\tEND IF;");
-                result.append("\nEND ").append(name).append(";");
+                result.append("\nEND " + name + ";");
 
                 break;
 
@@ -83,23 +142,23 @@ public class PLSQLTranslator implements Translator{
             	List<String> list = new ArrayList<String>(Arrays.asList(attributeListFunction.getList().split(",")));
                 int count = 0;
                 result.append("\nBEGIN");
-                result.append("\n\tIF (NOT (:new."+rule.getAttribute());
-                result.append(" "+translateOperator(attributeListFunction.getType())+" (");
-                for (String s : list){
-                    count+=1;
-                    if (isNumeric(s)==true){
+                result.append("\n\tIF (NOT (:new." + rule.getAttribute());
+                result.append(" " + translateOperator(attributeListFunction.getType()) + " (");
+                for (String s : list) {
+                    count++;
+                    if (isNumeric(s)) {
                     result.append(s);
                     }
-                    else{
-                    result.append("'"+s+"'");
-                    	}
-                    if (count < list.size()){
+                    else {
+                    result.append("\'" + s + "\'");
+                    }
+                    if (count < list.size()) {
                         result.append(",");
                     }
                 }
                 result.append("))) \n\tTHEN RAISE_APPLICATION_ERROR(-20000, \'" + name + " was triggered\');");
                 result.append("\n\tEND IF;");
-                result.append("\nEND ").append(name).append(";");
+                result.append("\nEND " + name + ";");
 
                 break;
 
@@ -107,26 +166,26 @@ public class PLSQLTranslator implements Translator{
                 Other attributeOtherFunction = ((Other) rule.getFunction());
 
                 result.append(attributeOtherFunction.getBody());
-                result.append(name).append(";");
+                result.append(name + ";");
                 break;
 
             case "TCMP" :
                 CompareOperator tupleCompareFunction = ((CompareOperator) rule.getFunction());
 
                 result.append("\nBEGIN");
-                result.append("\n\tIF (NOT (:new.").append(rule.getAttribute()).append(" ");
-                result.append(translateOperator(tupleCompareFunction.getType())).append(" :new.");
-                result.append(rule.getType().getOtherattribute()).append("))");
-                result.append("\n\tTHEN RAISE_APPLICATION_ERROR(-20000, \'").append(name).append(" was triggered\');");
+                result.append("\n\tIF (NOT (:new." + rule.getAttribute() + " ");
+                result.append(translateOperator(tupleCompareFunction.getType()) + " :new.");
+                result.append(rule.getType().getOtherattribute() + "))");
+                result.append("\n\tTHEN RAISE_APPLICATION_ERROR(-20000, \'" + name + " was triggered\');");
                 result.append("\n\tEND IF;");
-                result.append("\nEND ").append(name).append(";");
+                result.append("\nEND " + name + ";");
                 break;
 
             case "TOTH" :
                 Other tupleOtherFunction = ((Other) rule.getFunction());
 
                 result.append(tupleOtherFunction.getBody());
-                result.append(name).append(";");
+                result.append(name + ";");
                 break;
 
             case "ICMP" :
@@ -135,26 +194,27 @@ public class PLSQLTranslator implements Translator{
                 result.append("\n\tCURSOR lc IS");
                 result.append("\n\tSELECT TOP 1" + rule.getType().getOtherattribute());
                 result.append("\n\tFROM " + rule.getType().getOthertable());
-                result.append("\n\tl_" + rule.getType().getOtherattribute() + " " + rule.getType().getOthertable() + "." + rule.getType().getOtherattribute() + "%type");
+                result.append("\n\tl_" + rule.getType().getOtherattribute() + " " + rule.getType().getOthertable());
+                result.append("." + rule.getType().getOtherattribute() + "%type");
 
                 result.append("\nBEGIN");
                 result.append("\n\tOPEN lc;");
                 result.append("\n\tFETCH lc INTO l_" + rule.getType().getOtherattribute() + ";");
                 result.append("\n\tCLOSE lc;");
-                result.append("\n\tIF (NOT (:new.").append(rule.getTable()).append(".").append(rule.getAttribute());
-                result.append(" ").append(translateOperator(entityCompareFunction.getType()));
+                result.append("\n\tIF (NOT (:new." + rule.getTable() + "." + rule.getAttribute());
+                result.append(" " + translateOperator(entityCompareFunction.getType()));
                 result.append("l_" + rule.getType().getOtherattribute());
                 result.append("))");
-                result.append("\n\tTHEN RAISE_APPLICATION_ERROR(-20000, \'").append(name).append(" was triggered\');");
+                result.append("\n\tTHEN RAISE_APPLICATION_ERROR(-20000, \'" + name + " was triggered\');");
                 result.append("\n\tEND IF;");
-                result.append("\nEND ").append(name).append(";");
+                result.append("\nEND " + name + ";");
                 break;
 
             case "EOTH" :
                 Other entityOtherFunction = ((Other) rule.getFunction());
 
                 result.append(entityOtherFunction.getBody());
-                result.append(name).append(";");
+                result.append(name + ";");
                 break;
 
             default :
@@ -199,18 +259,6 @@ public class PLSQLTranslator implements Translator{
                 result = ">=";
                 break;
         }
-
-        return result;
-    }
-
-    public String generateDemo(Rule rule) {
-        String result = "";
-
-        result += "\nHERE BE CONSTRAINT OF TYPE " + rule.getType().getDesc() + "\n";
-        result += "\n\tAFTER UPDATE OR INSERT OR DELETE";
-        result += "\n\tON " + rule.getTable();
-        result += "\n\tCONCERNING " + rule.getAttribute() + "\n";
-        result += "\nCORRECT AND COMPLETE PL SQL STATEMENT WILL FOLLOW SOON(TM)";
 
         return result;
     }
